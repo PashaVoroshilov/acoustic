0f9dff518f7d72905df78e14a7da6c8c38a4b540
PashaVoroshilov <124685604+PashaVoroshilov@users.noreply.github.com>
2024-11-24 22:13:37 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,1 @@
+"use strict";document.addEventListener("DOMContentLoaded",function(){new Swiper(".greet-slider.swiper",{slidesPerView:1,pagination:{el:".greet-slider-actions__pagination.swiper-pagination",type:"fraction"},breakpoints:{992:{navigation:{nextEl:".greet-slider-actions__btn.slider-btn__next",prevEl:".greet-slider-actions__btn.slider-btn__prev"}}}})});
